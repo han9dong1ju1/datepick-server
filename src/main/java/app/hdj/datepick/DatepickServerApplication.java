b1f19dca8ce8dd49978825b7dfc21121389365d4
변영민 <dudals1303@g.skku.edu>
2021-07-22 19:10:37 +0900

    init

---
+++ src/main/java/app/hdj/datepick/DatepickServerApplication.java
@@ -0,0 +1,13 @@
+package app.hdj.datepick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatepickServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatepickServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/hdj/datepick/DatepickServerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.hdj.datepick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatepickServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
