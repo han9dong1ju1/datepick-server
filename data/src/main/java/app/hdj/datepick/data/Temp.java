52c17bac93984dbfbc07fa3314e792c9354b3b0b
변영민 <dudals1303@g.skku.edu>
2021-08-06 20:24:21 +0900

    [feature/DATEPICK-58]: service 패키지 이동 및 Lombok annotation 적용

--- data/src/main/java/app/hdj/datepick/data/Temp.java
+++
@@ -1,4 +0,0 @@
-package app.hdj.datepick.data;
-
-public class Temp {
-}
--- domain/src/main/java/app/hdj/datepick/domain/service/UserService.java
+++ domain/src/main/java/app/hdj/datepick/domain/service/UserService.java
@@ -1,8 +1,20 @@
 package app.hdj.datepick.domain.service;
 
+import app.hdj.datepick.domain.dto.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
+import java.util.List;
 
+@Service
+public interface UserService {
+    public List<User> findAll();
+    public Page<User> findAll(Pageable pageable);
+    public User findById(Long id);
+    public List<User> findByIdList(List<Long> idList);2
+
+    public Boolean create(User user);
+    public User update(User user);
+    public Boolean delete(User user);
 }
