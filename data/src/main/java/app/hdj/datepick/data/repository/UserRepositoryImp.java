37106ee9c764b3456e5d5f26f643946750264b06
변영민 <dudals1303@g.skku.edu>
2021-08-08 18:06:16 +0900

    [feature/DATEPICK-65]: 심플한 리팩토링

--- data/src/main/java/app/hdj/datepick/data/repository/UserRepositoryImp.java
+++ data/src/main/java/app/hdj/datepick/data/repository/UserRepositoryImp.java
@@ -4,7 +4,6 @@ import app.hdj.datepick.data.entity.UserTable;
 import app.hdj.datepick.data.query.JpaUserRepository;
 import app.hdj.datepick.domain.dto.User;
 import app.hdj.datepick.domain.repository.UserRepository;
-import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +13,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
@@ -23,22 +21,24 @@ import java.util.stream.Collectors;
 @Repository
 public class UserRepositoryImp implements UserRepository {
 
+    /**
+     * Entity to DTO Mapper
+     * (참고: https://stackoverflow.com/questions/47929674/modelmapper-mapping-list-of-entites-to-list-of-dto-objects/58324819)
+     */
     private final ModelMapper mapper = new ModelMapper();
 
     private final JpaUserRepository jpaUserRepository;
 
-    @Autowired //Jpa Repositiory 주입.
+    @Autowired
     public UserRepositoryImp(JpaUserRepository jpaUserRepository) {
         this.jpaUserRepository = jpaUserRepository;
     }
 
     @Override
     public List<User> findAll(){
-        //https://stackoverflow.com/questions/47929674/modelmapper-mapping-list-of-entites-to-list-of-dto-objects/58324819 참고
         List<UserTable> userTables = jpaUserRepository.findAll();
         Type listType = new TypeToken<List<User>>(){}.getType();
         List<User> users = mapper.map(userTables, listType);
-
         return users;
     }
 
@@ -64,9 +64,8 @@ public class UserRepositoryImp implements UserRepository {
 
     @Override
     public List<User> findByIdList(List<Long> idList){
-        List<User> users = new ArrayList<>();
-        users = idList.stream()
-                .map(id -> findById(id))
+        List<User> users = idList.stream()
+                .map(this::findById)
                 .map(userTable -> mapper.map(userTable, User.class))
                 .collect(Collectors.toList());
         return users;
@@ -74,11 +73,11 @@ public class UserRepositoryImp implements UserRepository {
 
     @Override
     public Boolean create(User user) {
-        try{
+        try {
             if (jpaUserRepository.existsById(user.getId())) {
                 throw new IllegalAccessException(String.format("해당 id : %d의 유저는 이미 존재합니다.", user.getId()));
             }
-        }catch (IllegalAccessException e) {
+        } catch (IllegalAccessException e) {
             //Exception handler
             return false;
         }
