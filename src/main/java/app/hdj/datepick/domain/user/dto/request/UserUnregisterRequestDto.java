1ff8cc9c7d22ed45bb608def423b8977f54cd3b9
Gongmeda <gongmeda@gmail.com>
2021-10-07 04:54:25 +0900

    [feature/DATEPICK-84]: User Authentication API(Controller) 구현

---
+++ src/main/java/app/hdj/datepick/domain/user/dto/request/UserUnregisterRequestDto.java
@@ -0,0 +1,19 @@
+package app.hdj.datepick.domain.user.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUnregisterRequestDto {
+
+    @NotNull
+    private String reason;
+
+}
