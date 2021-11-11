262339343c80a612b24dda45e11f83d8ff0927d3
kimhwijin <azxsdc0160@naver.com>
2021-11-11 14:28:32 +0900

    CourseMetaDto 변수 추가

--- src/main/java/app/hdj/datepick/domain/course/dto/CourseMetaDto.java
+++ src/main/java/app/hdj/datepick/domain/course/dto/CourseMetaDto.java
@@ -1,21 +1,33 @@
 package app.hdj.datepick.domain.course.dto;
 
 import app.hdj.datepick.domain.user.dto.UserMetaDto;
+import app.hdj.datepick.global.common.enums.Region;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CourseMetaDto {
-    Long id;
-    String title;
-    Integer pickCount;
-    Integer importCount;
-    Long userId;
+
+    private Long id;
+    private String title;
+    private String region;
+    private LocalDateTime expectedAt;
+    private Integer pickCount;
+    private Integer importCount;
+    private Long userId;
+    private String thumbNailUrl;
 
     @QueryProjection
-    public CourseMetaDto(Long id, String title, Integer pickCount, Integer importCount, Long userId) {
+    public CourseMetaDto(Long id, String title, String region, LocalDateTime expectedAt, Integer pickCount, Integer importCount, Long userId) {
         this.id = id;
         this.title = title;
+        this.region = region;
+        this.expectedAt = expectedAt;
         this.pickCount = pickCount;
         this.importCount = importCount;
         this.userId = userId;
