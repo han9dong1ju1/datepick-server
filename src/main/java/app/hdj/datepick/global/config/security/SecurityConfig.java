21a6bf544f6a25509f595b649c6f813fd90cedfe
Gongmeda <gongmeda@gmail.com>
2021-11-12 21:10:01 +0900

    [main]: Update SecurityConfig.java

--- src/main/java/app/hdj/datepick/global/config/security/SecurityConfig.java
+++ src/main/java/app/hdj/datepick/global/config/security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -72,5 +73,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authenticationProvider(firebaseAuthProvider());
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        // Auth Filter 스킵 설정
+        web.ignoring()
+                .antMatchers(HttpMethod.POST, "/v1/users/register");
     }
 }
