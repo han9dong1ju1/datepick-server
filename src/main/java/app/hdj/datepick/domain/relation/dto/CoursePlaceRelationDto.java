2de14d6634a9c79459a83095d219d8ea9642721e
kimhwijin <rlagnlwls0160@gmail.com>
2021-11-13 00:49:19 +0900

    findPlaceRelationDtoByCourseId 구현, QCoursePlaceRelationDto->QCoursePlaceDetailRelationDto, CoursePlaceRelationDto 생성, Diary Post, Patch, Delete 구현, Diary Service addDiary, deleteCourse, modifyDiary, checkValidationAddDiary 구현, ModifyPlaceReviewDto 생성, PlaceReopsitory findOrderAndPlaceInCourse 구현, PlaceWithOrderDto 생성

--- src/main/java/app/hdj/datepick/domain/relation/dto/CoursePlaceRelationDto.java
+++ src/main/java/app/hdj/datepick/domain/relation/dto/CoursePlaceRelationDto.java
@@ -1,29 +1,28 @@
-package app.hdj.datepick.domain.course.dto;
+package app.hdj.datepick.domain.relation.dto;
 
-import app.hdj.datepick.domain.place.dto.PlaceMetaDto;
+
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
-import lombok.ToString;
 
 import java.time.LocalDateTime;
 
-@ToString
 @Getter
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CoursePlaceRelationDto {
-
+    private Long courseId;
+    private Long placeId;
     private Byte placeOrder;
     private LocalDateTime visitTime;
     private String memo;
-    private PlaceMetaDto placeMeta;
 
     @QueryProjection
-    public CoursePlaceRelationDto(Byte placeOrder, LocalDateTime visitTime, String memo, PlaceMetaDto placeMeta) {
+    public CoursePlaceRelationDto(Long courseId, Long placeId, Byte placeOrder, LocalDateTime visitTime, String memo) {
+        this.courseId = courseId;
+        this.placeId = placeId;
         this.placeOrder = placeOrder;
         this.visitTime = visitTime;
         this.memo = memo;
-        this.placeMeta = placeMeta;
     }
 }
