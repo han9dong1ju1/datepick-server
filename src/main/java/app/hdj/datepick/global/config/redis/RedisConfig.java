71d80084a923fd9962d5b191e917cee4d0f31be1
Gongmeda <gongmeda@gmail.com>
2022-01-30 23:28:26 +0900

    [feature/DATEPICK-198]: redis 기본 세팅 보완 및 embedded redis 세팅

--- src/main/java/app/hdj/datepick/global/config/redis/RedisConfig.java
+++ src/main/java/app/hdj/datepick/global/config/redis/RedisConfig.java
@@ -1,34 +1,21 @@
 package app.hdj.datepick.global.config.redis;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.datasource.redis.host}")
-    private String host;
-
-    @Value("${spring.datasource.redis.port}")
-    private int port;
-
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(connectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
-
 
 }
