6fbfa197d9f4172fcdd43a07a3c142342cecb4d1
kimhwijin <rlagnlwls0160@gmail.com>
2022-02-02 22:53:55 +0900

    [feature/DATEPICK-204]: Enum Sort 추가, Controller RequestParam 추가

--- src/main/java/app/hdj/datepick/domain/featured/repository/FeaturedCustomRepositoryImpl.java
+++ src/main/java/app/hdj/datepick/domain/featured/repository/FeaturedCustomRepositoryImpl.java
@@ -1,28 +1,33 @@
-//package app.hdj.datepick.domain.featured.repository;
-//
-//import app.hdj.datepick.domain.course.dto.QCourseMetaDto;
-//import app.hdj.datepick.domain.featured.dto.FeaturedCourseDto;
-//import app.hdj.datepick.domain.featured.dto.QFeaturedCourseDto;
-//import com.querydsl.jpa.impl.JPAQueryFactory;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Repository;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.List;
-//
-//import static app.hdj.datepick.domain.course.entity.QCourse.course;
-//import static app.hdj.datepick.domain.relation.entity.QCourseFeaturedRelation.courseFeaturedRelation;
-//
-//@Slf4j
-//@Transactional(readOnly = true)
-//@RequiredArgsConstructor
-//@Repository
-//public class FeaturedCustomRepositoryImpl implements FeaturedCustomRepository {
-//
-//    private final JPAQueryFactory jpaQueryFactory;
-//
-//    @Override
+package app.hdj.datepick.domain.featured.repository;
+
+import app.hdj.datepick.domain.course.dto.QCourseMetaDto;
+import app.hdj.datepick.domain.featured.dto.FeaturedCourseDto;
+import app.hdj.datepick.domain.featured.dto.QFeaturedCourseDto;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static app.hdj.datepick.domain.course.entity.QCourse.course;
+import static app.hdj.datepick.domain.relation.entity.QCourseFeaturedRelation.courseFeaturedRelation;
+
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Repository
+public class FeaturedCustomRepositoryImpl implements FeaturedCustomRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<FeaturedCourseDto> findCourseInFeaturedById(Long featuredId) {
+        return null;
+    }
+
+    //    @Override
 //    public List<FeaturedCourseDto> findCourseInFeaturedById(Long featuredId) {
 //        return jpaQueryFactory
 //                .select(new QFeaturedCourseDto(
@@ -42,4 +47,4 @@
 //                .orderBy(courseFeaturedRelation.courseOrder.asc())
 //                .fetch();
 //    }
-//}
+}
