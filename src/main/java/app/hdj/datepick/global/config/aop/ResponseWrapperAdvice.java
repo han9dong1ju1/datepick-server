cb8e73604032660ada7a19ad7ba27c968a39e0f6
Gongmeda <gongmeda@gmail.com>
2022-02-02 12:39:04 +0900

    [feature/DATEPICK-186]: Response Wrapper 구현

---
+++ src/main/java/app/hdj/datepick/global/config/aop/ResponseWrapperAdvice.java
@@ -0,0 +1,33 @@
+package app.hdj.datepick.global.config.aop;
+
+import app.hdj.datepick.global.common.BaseResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice
+public class ResponseWrapperAdvice implements ResponseBodyAdvice<Object> {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        if (body == null) {
+            return new BaseResponse<>(null, null, null);
+        }
+        else if (returnType.getParameterType().equals(BaseResponse.class) ||
+                 returnType.getParameterType().equals(ResponseEntity.class)) {
+            return body;
+        }
+        return new BaseResponse<>(null, null, body);
+    }
+
+}
