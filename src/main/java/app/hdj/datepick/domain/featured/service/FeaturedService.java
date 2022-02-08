c6776723640f4d49b7a891fffb2fce1213368a32
Gongmeda <gongmeda@gmail.com>
2022-02-09 02:48:43 +0900

    [feature/DATEPICK-204]: featured api 로직 수정 및 정리
    - paging 쿼리 안날아가던 오류 해결
    - paging request 파라미터 공통 클래스 생성 및 대체
    - paging response 클래스 생성 및 대체
    - response에 사용되는 featured, paging response 클래스에 snake case 적용
    - 공통된  paging 처리를 위한 PageUtil 컴포넌트 구현
    - Enum Validation 어노테이션 개선
    - featured repository 로직 수정

--- src/main/java/app/hdj/datepick/domain/featured/service/FeaturedService.java
+++ src/main/java/app/hdj/datepick/domain/featured/service/FeaturedService.java
@@ -1,13 +1,14 @@
 package app.hdj.datepick.domain.featured.service;
 
-import app.hdj.datepick.domain.featured.dto.FeaturedPage;
-import app.hdj.datepick.domain.featured.dto.FeaturedPageRequest;
 import app.hdj.datepick.domain.featured.entity.Featured;
 import app.hdj.datepick.domain.featured.repository.FeaturedRepository;
-import app.hdj.datepick.global.error.enums.ErrorCode;
-import app.hdj.datepick.global.error.exception.CustomException;
+import app.hdj.datepick.global.common.CustomPage;
+import app.hdj.datepick.global.common.PagingParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -17,14 +18,22 @@ public class FeaturedService {
 
     private final FeaturedRepository featuredRepository;
 
-    public FeaturedPage getFeaturedPage(Boolean isPinned, Long courseId, FeaturedPageRequest featuredPageRequest) {
-        if (courseId == null){
-            return featuredRepository.findFeaturedPageByIsPinned(isPinned, featuredPageRequest);
-        }
-        return featuredRepository.findFeaturedPageByIsPinnedAndCourseId(isPinned, courseId, featuredPageRequest);
+    public CustomPage<Featured> getFeaturedPage(Boolean isPinned, Long courseId, PagingParam pagingParam) {
+        // 페이징된 데이터 가져옴
+        PageRequest pageRequest = PageRequest.of(pagingParam.getPage(), pagingParam.getSize(), Sort.by("createdAt").descending());
+        Page<Featured> featuredPage = featuredRepository.findFeaturedPage(isPinned, courseId, pageRequest);
+
+        // CustomPage로 말아서 반환
+        return new CustomPage<>(
+                featuredPage.getTotalElements(),
+                featuredPage.getTotalPages(),
+                featuredPage.getNumber(),
+                featuredPage.getContent()
+        );
     }
 
     public Featured getFeatured(Long featuredId) {
         return featuredRepository.findById(featuredId).orElseThrow();
     }
+
 }
