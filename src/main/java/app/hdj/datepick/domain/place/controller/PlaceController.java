8e6ccef30ae56c7de60e4453589a3892845d4059
kimhwijin <rlagnlwls0160@gmail.com>
2022-02-11 15:22:53 +0900

    [feature/DATEPICK-205]: PlaceMetaDto -> PlacePage, Response 형태에 맞게 변경

--- src/main/java/app/hdj/datepick/domain/place/controller/PlaceController.java
+++ src/main/java/app/hdj/datepick/domain/place/controller/PlaceController.java
@@ -1,15 +1,16 @@
 package app.hdj.datepick.domain.place.controller;
 
-import app.hdj.datepick.domain.place.dto.PlaceDetailDto;
-import app.hdj.datepick.domain.place.dto.PlaceMetaDto;
-import app.hdj.datepick.domain.place.dto.request.PlaceRequestDto;
+import app.hdj.datepick.domain.place.dto.PlacePage;
 import app.hdj.datepick.domain.place.entity.Place;
+import app.hdj.datepick.domain.place.param.PlaceFilterParam;
 import app.hdj.datepick.domain.place.service.PlaceService;
+import app.hdj.datepick.global.common.CustomPage;
+import app.hdj.datepick.global.common.PagingParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 
 @Slf4j
@@ -18,33 +19,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("v1/places")
 public class PlaceController {
 
-//    private final PlaceService placeService;
-//
-//    @GetMapping("/{placeId}")
-//    public PlaceDetailDto getPlace(@PathVariable Long placeId){
-//        return placeService.getPlace(placeId);
-//    }
-//
-//    @GetMapping("/{placeId}/photos")
-//    public Page<String> getPlacePhotos(@PathVariable Long placeId, Pageable pageable){
-//        return placeService.getPlaceImagePage(placeId, pageable);
-//    }
-//
-//    @GetMapping("")
-//    public Page<PlaceMetaDto> getPlaces(@RequestParam(value = "request_type") String requestType , Pageable pageable){
-//        if(requestType.equals("picked")){
-//            return placeService.getPickedPlacePage(pageable);
-//        }else if (requestType.equals("recommended")){
-//            return placeService.getRecommendedPlaceList(pageable);
-//        }else {
-//            //TODO RequestParam Validation Exception
-//            return null;
-//        }
-//    }
-//
-//    @PostMapping("/add")
-//    public Place addPlace(@RequestBody PlaceRequestDto placeRequestDto){
-//        return placeService.addPlace(placeRequestDto);
-//    }
+    private final PlaceService placeService;
+
+    @GetMapping("")
+    public CustomPage<PlacePage> getPlacePage(@RequestParam(value = "course_id", required = false) Long courseId,
+                                              @Valid PlaceFilterParam placeFilterParam,
+                                              @Valid PagingParam pagingParam
+                                        ){
+        return placeService.getPlacePage();
+    }
 
 }
