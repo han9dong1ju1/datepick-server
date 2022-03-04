bc21832a417ad8ef703a284c62ae2bea8e34b70f
kimhwijin <rlagnlwls0160@gmail.com>
2022-03-04 12:52:33 +0900

    [feature/DATEPICK-205]: place filtering, paging, tranform 수행, pagingUtil applySorting 추가

--- src/main/java/app/hdj/datepick/global/util/PagingUtil.java
+++ src/main/java/app/hdj/datepick/global/util/PagingUtil.java
@@ -1,14 +1,12 @@
 package app.hdj.datepick.global.util;
 
-import com.querydsl.core.ResultTransformer;
-import com.querydsl.core.group.GroupBy;
 import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.core.types.dsl.PathBuilderFactory;
 import com.querydsl.jpa.JPQLQuery;
-import com.querydsl.jpa.impl.JPAQuery;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.support.Querydsl;
 import org.springframework.stereotype.Component;
 
@@ -32,9 +30,8 @@ public class PagingUtil {
         return new PageImpl<>(results, pageable, totalCount);
     }
 
-    public <T> JPQLQuery<T> applySort(Pageable pageable, JPAQuery<T> query, Class classType){
-        return getQuerydsl(classType).applySorting(pageable.getSort(), query);
+    public  <T> JPQLQuery<T> applySorting(Sort sort, JPQLQuery<T> query, Class classType){
+        return getQuerydsl(classType).applySorting(sort, query);
     }
-
 
 }
