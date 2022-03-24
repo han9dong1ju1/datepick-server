77b3a5338743f7073993d94f0d8c0f93695cd1d8
Gongmeda <gongmeda@gmail.com>
2022-03-25 02:53:11 +0900

    [feature/DATEPICK-207]: Course api 마무리

--- src/main/java/app/hdj/datepick/domain/relation/repository/CoursePlaceRepository.java
+++ src/main/java/app/hdj/datepick/domain/relation/repository/CoursePlaceRepository.java
@@ -3,5 +3,8 @@ package app.hdj.datepick.domain.relation.repository;
 import app.hdj.datepick.domain.relation.entity.CoursePlaceRelation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CoursePlaceRepository extends JpaRepository<CoursePlaceRelation, Long> {
+    List<CoursePlaceRelation> findByCourseId(Long courseId);
 }
