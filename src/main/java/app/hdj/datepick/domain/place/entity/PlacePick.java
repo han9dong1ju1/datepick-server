63929c17b5d1252c115d6bfaae63d7252c3e36da
Gongmeda <gongmeda@gmail.com>
2022-05-06 00:48:30 +0900

    [refactor/DATEPICK-227)]: 불필요한 @JsonIgnore 어노테이션 제거

--- src/main/java/app/hdj/datepick/domain/place/entity/PlacePick.java
+++ src/main/java/app/hdj/datepick/domain/place/entity/PlacePick.java
@@ -2,7 +2,6 @@ package app.hdj.datepick.domain.place.entity;
 
 import app.hdj.datepick.domain.user.entity.User;
 import app.hdj.datepick.global.entity.BaseTimeEntity;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
@@ -17,7 +16,6 @@ public class PlacePick extends BaseTimeEntity {
     @Column
     private Long id;
 
-    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
